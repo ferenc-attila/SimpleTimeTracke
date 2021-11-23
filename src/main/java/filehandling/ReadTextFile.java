13bf4e104ac32c598cc84931568f4fa3b9ff7886
Attila <attila.ferenc.80@gmail.com>
2021-11-23 21:03:15 +0100

    Classes read and write text files added

---
+++ src/main/java/filehandling/ReadTextFile.java
@@ -0,0 +1,17 @@
+package filehandling;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class ReadTextFile {
+
+    public List<String> readTextFile(Path path) {
+        try {
+            return Files.readAllLines(path);
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Unable to read file: " + path + ";", ioe);
+        }
+    }
+}
---
+++ src/main/java/filehandling/WriteTextFile.java
@@ -0,0 +1,17 @@
+package filehandling;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class WriteTextFile {
+
+    public void writeTextFile(Path relativePath, String filename, List<String> stringList) {
+        try {
+            Files.write(relativePath.resolve(filename), stringList);
+        } catch (IOException ioe) {
+            throw new IllegalStateException("Unable to write file: " + relativePath.toString() + filename + ";", ioe);
+        }
+    }
+}
