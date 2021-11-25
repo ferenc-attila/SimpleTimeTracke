074e8e04dc8f52679c7bfc0a2f2e33649fe88588
Attila <attila.ferenc.80@gmail.com>
2021-11-25 23:01:44 +0100

    StartRecordingTests updated with new testmethod

--- src/test/java/service/recording/StartRecordingTest.java
+++ src/test/java/service/recording/StartRecordingTest.java
@@ -30,6 +30,20 @@ class StartRecordingTest {
     }
 
     @Test
+    void createIdentifierMaxIdNotInTheEndOfListTest() {
+        recordingList.addRecording(new Recording(3, "housework", LocalDateTime.now()));
+        recordingList.addRecording(new Recording(4, "project planning", LocalDateTime.now().minusDays(5)));
+        recordingList.addRecording(new Recording(2, "meeting", LocalDateTime.now().minusMonths(2)));
+        start.startRecording("hiking", recordingList);
+        assertEquals("hiking", recordingList.getRecordings().get(3).getDescription());
+        assertEquals(5, recordingList.getRecordings().get(3).getIdentifier());
+        recordingList.addRecording(new Recording(0, "go out with the dog", LocalDateTime.now().minusWeeks(2)));
+        start.startRecording("travel to Grandparents", recordingList);
+        assertEquals("travel to Grandparents", recordingList.getRecordings().get(5).getDescription());
+        assertEquals(6, recordingList.getRecordings().get(5).getIdentifier());
+    }
+
+    @Test
     void printStartMessageTest() {
         start.startRecording("morning commute", recordingList);
         String minuteNow = getMinuteNow();
