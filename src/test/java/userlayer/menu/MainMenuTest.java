1763a505bbbdd83598c6393f536801e745d709dc
Attila <attila.ferenc.80@gmail.com>
2021-11-27 10:14:04 +0100

    MainMenuTest created

---
+++ src/test/java/userlayer/menu/MainMenuTest.java
@@ -0,0 +1,14 @@
+package userlayer.menu;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainMenuTest {
+
+    @Test
+    void initializationTest() {
+
+
+    }
+}
